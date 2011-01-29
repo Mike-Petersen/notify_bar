d1cecf20ca14dd2ce3f62d30ffd0a6a8d1ced226
Mike Petersen <mike@odania-it.de>
2011-01-29 13:19:51 +0100

    Changes for Rails 3

--- lib/notify_bar/helper.rb
+++ lib/notify_bar/helper.rb
@@ -25,16 +25,15 @@ module NotifyBar
 
 		def notify_bar_js html, opts = {}
 			opts.reverse_merge! :delay => 2000, :animation_speed => 'normal', :close => false, :cls => 'notice'
-			%($(function () {
+			"$(function () {
 					$.notifyBar({
 						html: '#{escape_javascript(html)}',
 						delay: #{opts[:delay]},
-						animationSpeed: "#{opts[:animation_speed]}",
-						cls: "#{opts[:cls]}",
+						animationSpeed: '#{opts[:animation_speed]}',
+						cls: '#{opts[:cls]}',
 						close: #{opts[:close].to_s}
 						});
-					});
-			)
+					});".html_safe
 		end
 
 	end
